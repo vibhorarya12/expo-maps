52b3e42e6bfa51a81b15c5bfd76e81fefed4a2e1
Vibhor Arya <135149764+vibhorarya12@users.noreply.github.com>
2024-08-28 13:03:20 +0530

    maps layout created

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -32,6 +32,15 @@ export default function TabLayout() {
           ),
         }}
       />
+      <Tabs.Screen
+        name="maps"
+        options={{
+          title: 'Maps',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
---
+++ app/(tabs)/maps.tsx
@@ -0,0 +1,15 @@
+import { View , Text } from "react-native"
+
+const Maps = ()=>{
+
+   return(<View>
+    <Text>
+     this is maps
+    </Text>
+   </View>)
+
+
+
+}
+
+export default Maps;
